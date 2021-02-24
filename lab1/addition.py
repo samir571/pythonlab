79ac554197669ace6e9b3dcf54cd3cc41bef570f
samir571 <sameershrestha68@gmail.com>
2021-02-24 11:19:50 +0545

    Initial commit

---
+++ lab1/addition.py
@@ -0,0 +1,6 @@
+# write a program that takes three numbers and prints their sum. every number is given on a separate line.
+a = int(input('enter first a number'))
+b = int(input('enter the second number'))
+c = int(input('enter the third number'))
+sum = a + b + c
+print('the sum is', sum)
---
+++ lab1/question2.py
@@ -0,0 +1,1 @@
+# write a program that reads the length of the base and the height of a right angled triangle and prints the area. every number is given on a separate line.
