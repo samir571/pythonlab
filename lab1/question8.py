4405f4af939bc0897333a4b5c4cf5d02cccb8b59
samir571 <sameershrestha68@gmail.com>
2021-03-10 11:34:46 +0545

    sum of n positive number

---
+++ lab1/question8.py
@@ -0,0 +1,1 @@
+# write a python program which accepts the radius of circle from the user and compute area
---
+++ lab1/question9.py
@@ -0,0 +1,5 @@
+# write a python program to find the sum of the first n posiotive numbers
+# sum =(n*(n+1))/2
+n = int(input("enter a number"))
+sum = (n * (n+1))/2
+print(sum)
