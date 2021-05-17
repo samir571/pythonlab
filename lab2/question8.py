ec7919a3541b8c7a4e9dfc417eb3b550b171d918
samir571 <sameershrestha68@gmail.com>
2021-05-17 14:36:06 +0545

    change

--- lab2/question8.py
+++ lab2/question8.py
@@ -1,9 +1,16 @@
-a = float(input('enter a number'))
-b = a%10 #2
-c = a//10 #22
-d = c%10 #2
-e = c//10 #2
-f = e + b + d
-print(f"the sum is {f}")
+# a = float(input('enter a number'))
+# b = a%10 #2
+# c = a//10 #22
+# d = c%10 #2
+# e = c//10 #2
+# f = e + b + d
+# print(f"the sum is {f}")
+
+def fun(num):
+    if num<0:
+        return 0
+    else:
+        return fun(num-1)
+print(fun(5))
 
 
